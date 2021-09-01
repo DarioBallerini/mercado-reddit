38b53dba6135beda2c303162c86fd3d7fbf4f760
DarioBallerini <darballerini@gmail.com>
2021-09-01 16:39:41 -0300

    github pages settings

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
   productionSourceMap: false,
   configureWebpack: {
     devtool: 'source-map'
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/mercado-reddit/'
+    : '/'
 }
