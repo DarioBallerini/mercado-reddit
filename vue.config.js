9b86402d3d099789f93280d3ce10289659320983
DarioBallerini <darballerini@gmail.com>
2021-09-03 16:20:14 -0300

    fix deploy

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,5 @@ module.exports = {
   productionSourceMap: false,
   configureWebpack: {
     devtool: 'source-map'
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/mercado-reddit/'
-    : '/'
+  }
 }
