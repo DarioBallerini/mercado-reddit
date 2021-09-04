c4800936ba99b0ed38bae6b2db6d8bbfe6022359
DarioBallerini <darballerini@gmail.com>
2021-09-03 22:10:05 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueGtag from 'vue-gtag'
 
-const app = createApp(App)
-
-app.use(VueGtag, {
-  property: {
-    id: 'G-6P7WN4YNZ6'
-  }
-})
-
-app.mount('#app')
+createApp(App).use(VueGtag, {
+  config: { id: 'G-6P7WN4YNZ6' }
+}).mount('#app"')
