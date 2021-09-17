0b8b6ebb93d22bc2bd8f6c5c2a9730bcd5921680
DarioBallerini <darballerini@gmail.com>
2021-09-17 11:13:21 -0300

    added infinite scrolling

--- src/App.vue
+++ src/App.vue
@@ -24,16 +24,21 @@ export default defineComponent({
   },
   data () {
     return {
-      products: null,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      products: [] as any[],
       api: process.env.VUE_APP_API,
-      loading: true
+      loading: true,
+      searchQuery: '',
+      lastTime: 0
     }
   },
   created () {
     this.goHome()
+    this.getNextPage()
   },
   methods: {
     searchProducts (searchInput: string) {
+      this.searchQuery = searchInput
       if (!this.loading) {
         this.loading = true
         fetch(`https://www.reddit.com/r/Mercadoreddit/search.json?q=${searchInput}&restrict_sr=1&sort=new`)
@@ -46,12 +51,38 @@ export default defineComponent({
     },
     goHome () {
       this.loading = true
+      this.searchQuery = ''
       fetch(this.api)
         .then(response => response.json())
         .then(data => {
           this.products = data.data.children
           this.loading = false
         })
+    },
+    getNextPage () {
+      window.onscroll = () => {
+        const bottomOfWindow = document.documentElement.scrollTop + document.documentElement.offsetHeight + 600 >= (document.documentElement.scrollHeight) ||
+        document.documentElement.scrollTop + document.documentElement.offsetHeight >= (document.documentElement.scrollHeight)
+        const indexLastItem = this.products.length - 1
+        if (bottomOfWindow) {
+          var now = new Date().getTime()
+          if ((now - this.lastTime) < 3000) return
+          this.lastTime = now
+          if (this.searchQuery) {
+            fetch(`https://www.reddit.com/r/Mercadoreddit/search.json?q=${this.searchQuery}&after=${this.products[indexLastItem].kind}_${this.products[indexLastItem].data.id}&restrict_sr=1&sort=new`)
+              .then(response => response.json())
+              .then(data => {
+                this.products.push(...data.data.children)
+              })
+          } else {
+            fetch(`${this.api}?after=${this.products[indexLastItem].kind}_${this.products[indexLastItem].data.id}`)
+              .then(response => response.json())
+              .then((data) => {
+                this.products.push(...data.data.children)
+              })
+          }
+        }
+      }
     }
   }
 })
